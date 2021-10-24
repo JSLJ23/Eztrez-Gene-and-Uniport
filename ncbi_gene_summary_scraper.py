3f05de60ab9b114e431416982aa548b229ae5a23
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-10-24 21:05:21 +0800

    Requesting urls with 100 genes at a time from NCBI eutils API and extending list of tuples with (gene_IDs=, gene summary).

--- ncbi_gene_summary_scraper.py
+++ ncbi_gene_summary_scraper.py
@@ -2,7 +2,6 @@ import urllib
 import pandas as pd
 from urllib.request import urlopen
 import json
-from multiprocessing.pool import ThreadPool
 from datetime import datetime
 
 """
@@ -25,20 +24,27 @@ gene_ids_list = gene_ids.tolist()
 # Using list comprehension
 gene_ids_list_string = [str(x) for x in gene_ids_list]
 
-
-def get_chunks_in_list(input_list, chunk_size):
-    """ Returns generator object get_chunks_in_list """
-    return (input_list[pos:pos + chunk_size] for pos in range(0, len(input_list), chunk_size))
+gene_ids_list_string_small = gene_ids_list_string[0:300]  # For testing
 
 
 concatenated_list_Gene_IDs = []
+nested_list_Gene_IDs = []
+
+
+def get_chunks_in_list(input_list, chunk_size):
+    """
+    Returns generator object get_chunks_in_list
+    For slicing list of Gene IDs into nested lists of chunk_size
+    """
+    return (input_list[pos:pos + chunk_size] for pos in range(0, len(input_list), chunk_size))
 
 
 """
 
 """
-for chunks in get_chunks_in_list(gene_ids_list_string, 100):
+for chunks in get_chunks_in_list(gene_ids_list_string_small, 100):
     concatenated_list = ','.join(chunks)
+    nested_list_Gene_IDs.append(chunks)
     concatenated_list_Gene_IDs.append(concatenated_list)
 
 
@@ -52,14 +58,11 @@ def generate_url_list(gene_id):
 
 """ Enumerates through gene_ids_list_string to call generate_url_list and append generated urls to url_list """
 url_list = []
-for idx, gene in enumerate(gene_ids_list_string):
-    url_list.append(generate_url_list(gene))
+for idx, concat_gene_list in enumerate(concatenated_list_Gene_IDs):
+    url_list.append(generate_url_list(concat_gene_list))
 
 
-url_list_short = url_list[0:200]  # For small testing purposes, url list of first 100 elements
-
-
-def retrieve_ncbi_summary(url):
+def retrieve_ncbi_summary(url, list_of_gene_ids):
     """
     This functions takes in url as an argument and obtains the gene_id within the url.
     It then formats the origin of the request with a dummy browser, i.e. Mozilla.
@@ -67,26 +70,27 @@ def retrieve_ncbi_summary(url):
     If there isn't any summary for that particular Entrez ID, it returns "No gene summary in NCBI".
     Else, it returns the summary as a string of text.
     """
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    data = json.load(urllib.request.urlopen(req))
-    start = url.find("gene&id=") + len("gene&id=")
-    end = url.find("&retmode")
-    gene_id = url[start:end]
-    summary = data['result'][gene_id]['summary']
-    if len(summary) == 0 or summary is None:
-        return gene_id, "No gene summary in NCBI"
-    else:
-        return gene_id, summary
+    gene_ids_and_summaries = []
+    request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(request)
+    data = json.load(response)
+
+    for gene_id in list_of_gene_ids:
+        summary = data['result'][gene_id]['summary']
+        if len(summary) == 0 or summary is None:
+            summary_result = (gene_id, "No gene summary in NCBI")
+        else:
+            summary_result = (gene_id, summary)
+        gene_ids_and_summaries.append(summary_result)
+
+    return gene_ids_and_summaries, data
 
 
-""" A list for the results is initialized for the multiprocessing results to be appended to"""
-result_list = []
+results = []
 
-# Initialize a multiprocessing pool that will close after finishing execution.
-results = ThreadPool(1).imap_unordered(retrieve_ncbi_summary, url_list_short)
-
-for result in results:
-    result_list.append(result)
+for ncbi_url, ncbi_list_of_gene_ids in zip(url_list, nested_list_Gene_IDs):
+    returned_summaries = retrieve_ncbi_summary(ncbi_url, ncbi_list_of_gene_ids)
+    results.extend(returned_summaries)
 
 
 print(datetime.now() - startTime)
