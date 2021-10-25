e5ac5cbbec2cd1f4a075d1ca418d62c961978be1
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-10-25 13:41:46 +0800

    Tidied up Entrez summary dictionary generator function and added default key value pair.

--- gene_summary_dictionary.py
+++ gene_summary_dictionary.py
@@ -5,4 +5,5 @@ For generation of dictionary with key as Entrez ID of gene and value as Entrez s
 
 def generate_dictionary_from_tuple(results_in_tuple):
     gene_summary_dict = dict(results_in_tuple)
-    gene_summary_dict["default"] = "No gene summary available"}
+    gene_summary_dict["default"] = "No gene summary available"
+    return gene_summary_dict
