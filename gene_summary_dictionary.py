eeb2544bd96981e27893265dec1518c0cec3721d
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-10-25 10:58:37 +0800

    Adding gene_summary_dictionary module

---
+++ gene_summary_dictionary.py
@@ -0,0 +1,8 @@
+"""
+For generation of dictionary with key as Entrez ID of gene and value as Entrez summary scraped from NCBI's eutils API
+"""
+
+
+def generate_dictionary_from_tuple(results_in_tuple):
+    gene_summary_dict = dict(results_in_tuple)
+    gene_summary_dict["default"] = "No gene summary available"}
