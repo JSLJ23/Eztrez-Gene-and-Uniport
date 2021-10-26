282e94aa2eaa14fd9be7b023f9a5c89ceabe1438
Joshua Soon <mythicalwarrior.js@gmail.com>
2021-10-26 17:57:35 +0800

    Remove name of csv file in Usage_script.py

--- Usage_script.py
+++ Usage_script.py
@@ -1,4 +1,4 @@
 import ncbi_gene_summary_scraper
 
 # Sox8_KD_sig_DEGs_with_description
-ncbi_gene_summary_scraper.main("Sox8_KD_sig_DEGs_with_description")
+ncbi_gene_summary_scraper.main("insert CSV file name here")
